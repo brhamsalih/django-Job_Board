cbd3d35404aed7f916bf84a6626b8ef60a1624c6
brhamsalih <ibrahim.salih.f9@gmail.com>
2022-09-03 19:20:36 +0300

    new change

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my_APP
+    'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
+    'contact.apps.ContactConfig',
+    'home.apps.HomeConfig',
+    'job.apps.JobConfig',
 ]
 
 MIDDLEWARE = [
